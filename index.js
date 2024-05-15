3cdfd6c58ae95d1217587cd3a5578eaab0a20228
Fahmudul Hassan <fahmudul7890@email.com>
2024-05-15 14:07:08 +0600

    api secure with jwt token

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
 const express = require("express");
 const cors = require("cors");
+var jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 // middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("Hello From CareerCompass");
 });
+// Verify token
+const verifyToken = (req, res, next) => {
+  const token = req?.cookies?.token;
+  console.log("coming token from client", token);
+  if (!token) {
+    return res.status(401).send({ message: "unathorized access" });
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decode) => {
+    if (err) {
+      return res.status(401).send({ message: "unauthorized access" });
+    }
+    req.user = decode;
+    next();
+  });
+};
+
 // MongoDB functionalities
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nkzn5jr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -21,6 +45,11 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   },
 });
+const cookieOptions = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+};
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -39,8 +68,34 @@ async function run() {
       const allJobsList = await allCategoryJobCollection.find().toArray();
       res.send(allJobsList);
     });
+
+    // Token configuration and set cookie
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      console.log("email from jwt", user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "10d",
+      });
+      // console.log(token);
+      res.cookie("token", token, cookieOptions).send({ success: true });
+    });
+    // clear cookie
+    // app.post("/logout", async (req, res) => {
+    //   console.log('email from logout',req.body)
+    //   res
+    //     .clearCookie("token", { ...cookieOptions, maxAge: 0 })
+    //     .send({ success: true });
+    // });
+    app.post("/logout", async (req, res) => {
+      const user = req.body;
+      console.log("logging out", user);
+      res
+        .clearCookie("token", { maxAge: 0, sameSite: "none", secure: true })
+        .send({ success: true });
+    });
+
     // Get single job data
-    app.get("/allJobsCategory/:id", async (req, res) => {
+    app.get("/allJobsCategory/:id", verifyToken, async (req, res) => {
       const id = req.params.id;
       // console.log(typeof id);
       const query = { _id: new ObjectId(id) };
@@ -48,15 +103,18 @@ async function run() {
       res.send(result);
     });
     // Get my jobs
-    app.get("/myPostedJobs", async (req, res) => {
+    app.get("/myPostedJobs", verifyToken, async (req, res) => {
       // console.log(req.query.email)
       const email = req.query.email;
+      if (email !== req.user.email) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
       const filter = { ownerEmail: email };
       const result = await allCategoryJobCollection.find(filter).toArray();
       res.send(result);
     });
     // routes for job summary page
-    app.get("/appliedApplicantsDetails/:id", async (req, res) => {
+    app.get("/appliedApplicantsDetails/:id", verifyToken, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await appliedJobsApplicantCollection.findOne(query);
@@ -104,8 +162,13 @@ async function run() {
       res.send(result);
     });
     // get applied applicants list
-    app.get("/appliedApplicants", async (req, res) => {
+    app.get("/appliedApplicants", verifyToken, async (req, res) => {
       const email = req.query.email;
+      console.log(email);
+      console.log(req.user);
+      if (email !== req.user.email) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
       const filter = { email };
       const result = await appliedJobsApplicantCollection
         .find(filter)
